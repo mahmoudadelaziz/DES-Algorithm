40b01b80c27ec7e42f48a689ba10a7c356adb272
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-07 15:16:14 -0400

    Created the halving functions using bitsets in C++

--- C++ Program/text_to_binary.cpp
+++ C++ Program/text_to_binary.cpp
@@ -3,38 +3,75 @@
 
 using namespace std;
 
-#define BIT_ARRAY_LENGTH 64
+#define DATABLOCK_BIT_LENGTH 64
 
-int main()
+bitset<64> data_to_blocks(string ourPlainText)
 {
-    // Transform input text (plaintext) to 64-bit blocks
-
-    // Input string (plaintext)
-    string plainText;
-    cin >> plainText;
-    // Initiate binary string
+    /*
+    This function takes the input plaintext,
+    and returns a 64-bit data block.
+    */
     string binaryString;
     for (int i = 0; i < 8; i++)
     {
-        bitset<8> ch(plainText[i]);
+        bitset<8> ch(ourPlainText[i]);
         binaryString += ch.to_string();
     }
-    
-    bitset <64> dataBlock(binaryString);
 
-    cout << "The word is:   " << plainText << endl;
-    cout << "In bits:       " << dataBlock << endl;
+    bitset<64> dataBlock(binaryString);
+    return dataBlock;
+}
 
-    // Divide into two equal halves
-    bitset <32> leftSide_data(binaryString.substr(0,32));
-    bitset <32> rightSide_data(binaryString.substr(32,32));
+bitset<32> leftSide(bitset<64> ourData)
+{
+    // Returns the left side of our input plaintext data block
+    bitset <32> leftSide_data(ourData.to_string().substr(0, 32));
+    return leftSide_data;
+}
 
-    //test
-    bitset <32> testing (0x00);
+bitset<32> rightSide(bitset<64> ourData)
+{
+    // Returns the right side of our input plaintext data block
+    bitset <32> rightSide_data(ourData.to_string().substr(32, 32));
+    return rightSide_data;
+}
 
-    cout << "The right side of the data block: " << rightSide_data << endl;
-    cout << "The left side of the data block:  " << leftSide_data << endl;
+int main()
+{
 
-    cout << "Size of our data block = " << dataBlock.size() << " bits." << endl;
-    cout << "Size of each side = " << testing.size() << " bits." << endl;
+    string Word = "Universe";
+    cout << data_to_blocks(Word) << endl;
+    cout << leftSide(data_to_blocks(Word)) << endl;
+    cout << rightSide(data_to_blocks(Word)) << endl;
+
+    // Transform input text (plaintext) to 64-bit blocks
+
+    // // Input string (plaintext)
+    // string plainText;
+    // cin >> plainText;
+    // // Initiate binary string
+    // string binaryString;
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     bitset<8> ch(plainText[i]);
+    //     binaryString += ch.to_string();
+    // }
+
+    // bitset<64> dataBlock(binaryString);
+
+    // cout << "The word is:   " << plainText << endl;
+    // cout << "In bits:       " << dataBlock << endl;
+
+    // // Divide into two equal halves
+    // bitset<32> leftSide_data(binaryString.substr(0, 32));
+    // bitset<32> rightSide_data(binaryString.substr(32, 32));
+
+    // // test
+    // bitset<32> testing(0x00);
+
+    // cout << "The right side of the data block: " << rightSide_data << endl;
+    // cout << "The left side of the data block:  " << leftSide_data << endl;
+
+    // cout << "Size of our data block = " << dataBlock.size() << " bits." << endl;
+    // cout << "Size of each side = " << testing.size() << " bits." << endl;
 }
