2227d9de942ef2f60739099d408d568d2056be48
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-10 00:04:01 -0400

    Added the PC_2 table for the Key.

--- Code.cpp
+++ Code.cpp
@@ -39,6 +39,18 @@ int keyPermutationChoice_1_Table[56] =
      14, 6, 61, 53, 45, 37, 29,
      21, 13, 5, 28, 20, 12, 4};
 
+// Indices for Permutation Choice 2 for the Key
+int keyPermutationChoice_2_Table[48] = 
+    {14, 17, 11, 24, 1, 5, 
+	3, 28, 15, 6, 21, 10, 
+	23, 19, 12, 4, 26, 8, 
+	16, 7, 27, 20, 13, 2, 
+	41, 52, 31, 37, 47, 55, 
+	30, 40, 51, 45, 33, 48, 
+	44, 49, 39, 56, 34, 53, 
+	46, 42, 50, 36, 29, 32 
+	};
+
 bitset<64> data_to_blocks(string ourPlainText)
 {
     /*
