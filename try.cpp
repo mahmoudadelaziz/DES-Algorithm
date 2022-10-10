db333281a698d4f5bed8208f65636ea18f1f6876
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-10 17:16:33 -0400

    Wrote the subkey (Ki) generating function and tested it against the example.

--- try.cpp
+++ try.cpp
@@ -22,6 +22,16 @@ int keyPermutationChoice_1_Table[56] =
      14, 6, 61, 53, 45, 37, 29,
      21, 13, 5, 28, 20, 12, 4};
 
+int keyPermutationChoice_2_Table[48] =
+    {14, 17, 11, 24, 1, 5,
+     3, 28, 15, 6, 21, 10,
+     23, 19, 12, 4, 26, 8,
+     16, 7, 27, 20, 13, 2,
+     41, 52, 31, 37, 47, 55,
+     30, 40, 51, 45, 33, 48,
+     44, 49, 39, 56, 34, 53,
+     46, 42, 50, 36, 29, 32};
+
 bitset<28> Key_PC1_Left(bitset<64> fullKey)
 {
     /*
@@ -60,6 +70,38 @@ bitset<28> circularLeftShift(bitset<28> halfKey, int number_of_shifts)
     return roundShiftedHalfKey;
 }
 
+// Generate subkey Ki
+bitset<48> subKey(bitset<28> Ci, bitset<28> Di)
+{
+    /*
+    Takes the two 28-bit halves of the key after left-shifting (Ci, Di),
+    and returns the 48-bit subKey of the current DES round.
+    */
+
+    // First, combine C1 and D1
+    bitset<56> CDi(0);
+    for (int i = 0; i < 28; i++)
+    {
+        CDi[i] = Di[i];
+        CDi[28 + i] = Ci[i];
+    }
+
+    bitset<48> subKey_i(0);
+    for(int i = 1; i < 48; i++)
+    {
+        subKey_i[48 - i] = CDi[56 - keyPermutationChoice_2_Table[i-1]];
+    }
+
+    // for (int i = 1; i < 28; i++)
+    // {
+    //     // Right part of Ki
+    //     subKey_i[i - 1] = Di[27 - keyPermutationChoice_2_Table[i - 1]];
+    //     // Left part of Ki (prints all zeros?! Why?!)
+    //     // subKey_i[28 + i - 1] = Ci[28 - keyPermutationChoice_2_Table[28 + i - 1]];
+    // }
+    return subKey_i;
+}
+
 int main()
 {
     bitset<64> fullKey(0x133457799BBCDFF1);
@@ -74,6 +116,21 @@ int main()
     cout << "(Circular) left shift D0 by one: " << circularLeftShift(Key_PC1_Right(fullKey), 1) << endl;
     cout << "It should be:                    "
          << "1010101011001100111100011110" << endl;
+
+    // Generate subkey K1
+
+
+    bitset<56> CDi(0);
+    for (int i = 0; i < 28; i++)
+    {
+        CDi[i] = circularLeftShift(Key_PC1_Left(fullKey), 1)[i];
+        CDi[28 + i] = circularLeftShift(Key_PC1_Right(fullKey), 1)[i];
+    }
+    cout << "D1 and C1 combined: " << CDi << endl;
+    
+    cout << "K1:           " << subKey(circularLeftShift(Key_PC1_Left(fullKey), 1), circularLeftShift(Key_PC1_Right(fullKey), 1)) << endl;
+    cout << "K1 Should be: " << "000110110000001011101111111111000111000001110010" << endl;
+    
     // playing with circular loops
     // cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
     // cout << "Playing with circular arrays... Exploring!" << endl;
