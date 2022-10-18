c0353bbd02eeb74bdca9b3daa4693afe775dc938
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-18 13:09:30 -0400

    Got stuck on initial permutation for the same reason.

--- FileReading.cpp
+++ FileReading.cpp
@@ -5,6 +5,17 @@ using namespace std;
 
 typedef unsigned long long ull;
 
+int IP_Table[64] =
+    {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7}; // Indices for the Initial Permuation
+
 int main()
 {
     // Declare a file variable
@@ -27,14 +38,29 @@ int main()
     for (int i = 0; i < 8; i++)
     {
         // Characters in reverse order from the array
-        dataBlock |= (ull) ch[7-i] << (8 * i);
-        
+        dataBlock |= (ull)ch[7 - i] << (8 * i);
+
         // Debugging (you could see the block filling up!)
-        cout << "Iteration " << i+1 << ": ";
+        cout << "Iteration " << i + 1 << ": ";
         for (int j = 0; j < 64; j++)
             cout << ((dataBlock & 1UL << (63 - j)) != 0);
         cout << endl;
     }
+
+    /* Trying out initial permutation */
+    ull message = 0x0123456789ABCDEF; // from example
+    ull result = 0;
+
+    for (int i = 0; i < 64; i++)
+        result |= (((message & 1UL << (IP_Table[i] - 1)) != 0) << i);
+
+    cout << "Message: ";
+    for (int i = 0; i < 64; i++)
+        cout << ((message & 1UL << (63 - i)) != 0);
+
+    cout << "\nafter IP: \n";
+    for (int i = 0; i < 64; i++)
+        cout << ((result & 1UL << (63 - i)) != 0);
 
     return 0;
 }
--- initial_permutation.cpp
+++ initial_permutation.cpp
@@ -5,37 +5,34 @@
 */
 
 #include <iostream>
-#include <bits/stdc++.h>
+
+typedef unsigned long long ull;
+
+int IP_Table[64] =
+    {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7}; // Indices for the Initial Permuation
 
 using namespace std;
 
 int main()
 {
-    bitset<64> dataBlock("0000000100100011010001010110011110001001101010111100110111101111"); // Example
-    cout << "The message in binary (before IP): " << dataBlock << endl;
-
-    int IP_Table[64] =
-        {
-            58,    50,   42,    34,    26,   18,    10,    2,
-            60,    52,   44,    36,    28,   20,    12,    4,
-            62,    54,   46,    38,    30,   22,    14,    6,
-            64,    56,   48,    40,    32,   24,    16,    8,
-            57,    49,   41,    33,    25,   17,     9,    1,
-            59,    51,   43,    35,    27,   19,    11,    3,
-            61,    53,   45,    37,    29,   21,    13,    5,
-            63,    55,   47,    39,    31,   23,    15,    7
-        }; // Indices for the Initial Permuation
+    //cout << "The message in binary (before IP): " << dataBlock << endl;
 
     // Performing IP
-    bitset<64> data_after_IP(0);
-    bitset<32> L0 (0);
-    bitset<32> R0 (0);
+    ull result = 0;
 
     for (int i = 1; i < 65; i++)
     {
-        data_after_IP[64 - i] = dataBlock[64 - IP_Table[i - 1]]; //Redundant?
-        L0[32-i] = dataBlock[32 - IP_Table[i - 1]];
-        R0[32-i] = dataBlock[64 - IP_Table[32 + i - 1]];
+        result[64 - i] = dataBlock[64 - IP_Table[i - 1]]; // Redundant?
+        L0[32 - i] = dataBlock[32 - IP_Table[i - 1]];
+        R0[32 - i] = dataBlock[64 - IP_Table[32 + i - 1]];
     }
 
     // Working through the example
