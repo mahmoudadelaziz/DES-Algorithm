c0a866872ddf0eb7942931f19483b51a60e7ac5b
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-24 03:53:25 -0400

    Successfully generated K1

--- GeneratingKeys.cpp
+++ GeneratingKeys.cpp
@@ -11,6 +11,16 @@ int PC1_Table[56] =
      14, 6, 61, 53, 45, 37, 29,
      21, 13, 5, 28, 20, 12, 4}; // list of indices
 
+int PC2_Table[48] =
+    {14, 17, 11, 24, 1, 5,
+     3, 28, 15, 6, 21, 10,
+     23, 19, 12, 4, 26, 8,
+     16, 7, 27, 20, 13, 2,
+     41, 52, 31, 37, 47, 55,
+     30, 40, 51, 45, 33, 48,
+     44, 49, 39, 56, 34, 53,
+     46, 42, 50, 36, 29, 32};
+
 u64 Key_PC1(u64 fullKey)
 {
     /* takes the full 64-bit key,
@@ -18,6 +28,16 @@ u64 Key_PC1(u64 fullKey)
     u64 newKey = 0;
     for (int i = 0; i < 56; i++)
         newKey |= (fullKey >> (64 - PC1_Table[56 - 1 - i]) & 1) << i;
+    return newKey;
+}
+
+u64 Key_PC2(u64 CnDn)
+{
+    /* takes the CnDn (after bit-rotation),
+    and returns a 48-bit subkey for the current DES round. */
+    u64 newKey = 0;
+    for (int i = 0; i < 48; i++)
+        newKey |= (CnDn >> (56 - PC2_Table[48 - 1 - i]) & 1) << i;
     return newKey;
 }
 
@@ -68,5 +88,14 @@ int main()
     cout << "\nC1D1:\n";
     for (int i = 0; i < 64; i++)
         cout << ((subkey_before_PC2 & 1UL << (63 - i)) != 0);
+
+    u64 K1 = Key_PC2(subkey_before_PC2);
+
+    cout << "\nK1:\n";
+    for (int i = 0; i < 64; i++)
+        cout << ((K1 & 1UL << (63 - i)) != 0);
+
+
+
     return 0;
 }
