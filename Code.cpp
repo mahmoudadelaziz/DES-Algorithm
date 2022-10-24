9428b63feba91b217a52eb79e48e3166061560be
Mahmoud Aziz <mahmoudaziz501@gmail.com>
2022-10-24 05:20:45 -0400

    Started correcting and organizing the main code file.

--- Code.cpp
+++ Code.cpp
@@ -9,10 +9,12 @@
 */
 
 #include <iostream>
-#include <bits/stdc++.h>
 
 using namespace std;
 
+typedef unsigned long long u64;
+
+/* --------------- DEFINING GLOBAL VARIABLES --------------- */
 // Indices for the Initial Permuation
 int IP_Table[64] =
     {
@@ -48,7 +50,7 @@ int PC1_Table[56] =
      21, 13, 5, 28, 20, 12, 4};
 
 // Indices for Permutation Choice 2 for the Key
-int keyPermutationChoice_2_Table[48] =
+int PC2_Table[48] =
     {14, 17, 11, 24, 1, 5,
      3, 28, 15, 6, 21, 10,
      23, 19, 12, 4, 26, 8,
@@ -61,116 +63,6 @@ int keyPermutationChoice_2_Table[48] =
 // int shifts[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 int shifts[16] = {1, 2, 4, 6, 8, 10, 12, 14, 15, 17, 19, 21, 23, 25, 27, 28};
 
-int EP_Table[48] =
-    {32, 1, 2, 3, 4, 5, 4, 5,
-     6, 7, 8, 9, 8, 9, 10, 11,
-     12, 13, 12, 13, 14, 15, 16, 17,
-     16, 17, 18, 19, 20, 21, 20, 21,
-     22, 23, 24, 25, 24, 25, 26, 27,
-     28, 29, 28, 29, 30, 31, 32, 1};
-
-bitset<64> data_to_blocks(string ourPlainText)
-{
-    /*
-    This function takes the input plaintext,
-    and returns a 64-bit data block.
-    */
-    string binaryString;
-    for (int i = 0; i < 8; i++)
-    {
-        bitset<8> ch(ourPlainText[i]);
-        binaryString += ch.to_string();
-    }
-
-    bitset<64> dataBlock(binaryString);
-    return dataBlock;
-}
-
-bitset<32> L0(bitset<64> Data_after_IP)
-{
-    /*
-    Takes the 64-bit data block,
-    and returns the left side after performing Initial Permutation.
-    */
-    bitset<32> L0(0);
-    for (int i = 1; i < 65; i++)
-        L0[32 - i] = Data_after_IP[32 - IP_Table[i - 1]];
-    return L0;
-}
-
-bitset<32> R0(bitset<64> Data_after_IP)
-{
-    /*
-    Takes the 64-bit data block,
-    and returns the right side after performing Initial Permutation.
-    */
-    bitset<32> R0(0);
-    for (int i = 1; i < 65; i++)
-        R0[32 - i] = Data_after_IP[64 - IP_Table[32 + i - 1]];
-    return R0;
-}
-
-bitset<28> Key_PC1_Left(bitset<64> fullKey)
-{
-    /*
-    Takes the 64-bit key block,
-    and returns the left side after performing Permutation Choice 1 (C0).
-    */
-    bitset<28> L0(0);
-    for (int i = 1; i < 57; i++)
-        L0[28 - i] = fullKey[64 - PC1_Table[i - 1]];
-    return L0;
-}
-
-bitset<28> Key_PC1_Right(bitset<64> fullKey)
-{
-    /*
-    Takes the 64-bit key block,
-    and returns the right side after performing Permutation Choice 1 (D0).
-    */
-    bitset<28> R0(0);
-    for (int i = 1; i < 57; i++)
-        R0[28 - i] = fullKey[64 - PC1_Table[28 + i - 1]];
-    return R0;
-}
-
-// Circular left shift
-bitset<28> circularLeftShift(bitset<28> halfKey, int number_of_shifts)
-{
-    /*
-    Takes a half of the key after PC1 (C0, or D0),
-    and a number of shifts (either 1 or 2) that depends on which round we're in,
-    and performs left-shift on it (as required by the DES round)
-    */
-    bitset<28> roundShift_HalfKey(0);
-    for (int i = number_of_shifts; i < 28 + number_of_shifts; i++)
-        roundShift_HalfKey[i - number_of_shifts] = halfKey[((26 * number_of_shifts) + i) % 28];
-    return roundShift_HalfKey;
-}
-
-bitset<48> generate_subKey(bitset<28> Ci, bitset<28> Di)
-{
-    /*
-    Takes the two 28-bit halves of the key after left-shifting (Ci, Di),
-    and returns the 48-bit subKey of the current DES round.
-    */
-
-    // First, combine C1 and D1
-    bitset<56> CDi(0);
-    for (int i = 0; i < 28; i++)
-    {
-        CDi[i] = Di[i];
-        CDi[28 + i] = Ci[i];
-    }
-
-    bitset<48> subKey_i(0);
-    for (int i = 1; i < 49; i++)
-        subKey_i[48 - i] = CDi[56 - keyPermutationChoice_2_Table[i - 1]];
-
-    return subKey_i;
-}
-
-////////////////////////  Function block  /////////////////////////////////////
 int expansionPermutation_Table[48] = {
     32, 1, 2, 3, 4, 5,
     4, 5, 6, 7, 8, 9,
@@ -235,115 +127,26 @@ int S8[64] = {
 // Make an array of S-boxes to iterate through
 int *SBox_number[8] = {S1, S2, S3, S4, S5, S6, S7, S8};
 
-bitset<48> expansion_Permutation(bitset<32> old)
+/* --------------- DEFINING GLOBAL VARIABLES COMPLETE --------------- */
+
+/* ------------------ DEFINING HELPER FUNCTIONS --------------------------- */
+
+u64 do_initial_permutation(u64 M)
 {
-    bitset<48> result(0);
-    for (int i = 1; i < 49; i++)
-        result[48 - i] = old[(32) - (EP_Table[(i - 1)])];
+    u64 result = 0;
+    for (int i = 0; i < 64; ++i)
+        result |= (M >> (64 - IP_Table[i]) & 1) << 64 - (i + 1);
     return result;
-};
-
-bitset<32> simplePermutation(bitset<32> old)
-{
-    bitset<32> result(0);
-    for (int i = 1; i < 33; i++)
-        result[32 - i] = old[32 - simplePermutation_table[i - 1]];
-    return result;
-};
-
-bitset<32> S_Box(bitset<48> old)
-{
-    // Index components:
-    int row = 0;
-    int column = 0;
-    int table_no = 0;
-    bitset<32> result;
-    bitset<6> block;
-    for (int i = 0; i < 48; i += 6)
-    {
-        block.reset();
-        for (int j = i; j < i + 6; j++)
-        {
-            block[j - i] = old[j];
-        }
-        // Each 6-bit block defines the S-box index
-        // Table: number of the block we're on (i/6)
-        // Row: LSB [0] and MSB [5] of the block
-        row = 1 * block[0] + 2 * block[5];
-        // Column: inner bits of the block
-        column = 1 * block[1] + 2 * block[2] + 4 * block[3] + 8 * block[4];
-        // Index will look like: [column + 16 * row + (64 * table)]
-        bitset<4> target(SBox_number[7 - table_no][column + 16 * row]);
-        for (int k = 0; k < 4; k++)
-        {
-            result[k + table_no * 4] = target[k];
-        }
-        table_no++;
-    }
-    return result;
-};
-
-bitset<64> do_inversePermutation(bitset<32> left, bitset<32> right)
-{
-    bitset<64> result(0);
-    for (int i = 1; i < 33; i++)
-    {
-        result[32 - i] = right[32 - inversePermutation[i - 1]];
-        result[64 - i] = left[32 - inversePermutation[32 + i - 1]];
-    }
-    return result;
-}
-
-// bitset <64> do_inversePermutation(bitset<32> L_16, bitset<32> R_16)
-
-bitset<48> generate_roundKey(bitset<28> C0, bitset<28> D0, int round_number)
-{
-
-    bitset<28> C_i = circularLeftShift(C0, shifts[round_number]);
-    bitset<28> D_i = circularLeftShift(D0, shifts[round_number]);
-    // K_i = generate_subKey(C_i, D_i);
-    return generate_subKey(C_i, D_i);
 }
 
 int main()
 {
-    // Key preparation (WORKING WELL!)
-    // bitset<64> example_key("0001001100110100010101110111100110011011101111001101111111110001");
-    // bitset<28> C0 = Key_PC1_Left(example_key);
-    // bitset<28> D0 = Key_PC1_Right(example_key);
-    // // Key generation loop
-    // for (int round = 1; round < 17; round++)
-    // {
-    //     bitset<48> Ki = generate_roundKey(C0, D0, (round - 1));
-    //     cout << "K" << round << " = " << Ki << endl;
-    // }
+    // Initial Permutation
+    u64 after_IP = do_initial_permutation(81985529216486895);
+    // (Show result)
+    cout << "IP_M:";
+    for (int i = 0; i < 64; i++)
+        cout << ((after_IP & 1UL << (63 - i)) != 0);
 
-    // // -------------------- Testing Text functions --------------------
-    // bitset<64> example_M("0000000100100011010001010110011110001001101010111100110111101111");
-    // // Initial Permutation (IP)
-    // bitset<32> L_0 = L0(example_M);
-    // bitset<32> R_0 = R0(example_M);
-    // cout << "L0: " << L_0 << endl;
-    // cout << "R0: " << R_0 << endl;
-    // // Expansion Permutation (EP)
-    // bitset<48> E_R0 = expansion_Permutation(R_0);
-    // cout << "E(R0): " << E_R0 << endl;
-    // // -------------------- Testing Text functions --------------------
-    // // cout << "E(R0) XOR (K1) = " << ((E_R0) ^ (K1)) << endl;
-
-    // // S-Box function
-    // // Hard-coded E(R0) XOR K1
-    // bitset<48> sBox_input("011000010001011110111010100001100110010100100111");
-    // cout << "S-Box output: " << S_Box(sBox_input) << endl;
-
-    // // Simple Permutation
-    // cout << "F:            " << simplePermutation(S_Box(sBox_input)) << endl;
-
-    // Moving on...
-
-    // IP-1 test
-    bitset<32> L16("01000011010000100011001000110100");
-    bitset<32> R16("00001010010011001101100110010101");
-
-    cout << do_inversePermutation(R16, L16) << endl;
+    return 0;
 }
